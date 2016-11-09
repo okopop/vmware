6ce5d936d6512be875c1c470b5b0b04c860f0e77
okopop <ukusic@gmail.com>
2016-11-09 23:29:01 +0100

    Create probe_ova_for_prop_values.ps1

---
+++ probe_ova_for_prop_values.ps1
@@ -0,0 +1,11 @@
+# Probe ova file for prop values
+# Mattias Ukusic, 2016
+
+$OVTOOL_PATH="C:\X\ovftool.exe"
+
+$ova = Read-Host -Prompt 'Enter ova file name path'
+
+& $OVTOOL_PATH $ova
+
+$HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
+$HOST.UI.RawUI.Flushinputbuffer()
