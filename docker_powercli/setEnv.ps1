8a503b5a24b43fdc402145bd114f947f7b623f77
OKO <ukusic@gmail.com>
2016-11-15 23:52:22 +0100

    Delete setEnv.ps1

--- docker_powercli/setEnv.ps1
+++
@@ -1,8 +0,0 @@
-# ignore cert
-Set-PowerCLIConfiguration -InvalidCertificateAction ignore -confirm:$false
-
-# connect to vcsa
-$vcsa_ip = ""
-$vcsa_user = ""
-$vcsa_pass = ""
-Connect-VIServer -server $vcsa_ip -user $vcsa_user -password $vcsa_pass
