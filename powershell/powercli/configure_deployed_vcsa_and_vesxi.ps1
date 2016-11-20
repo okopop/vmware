4763ce473b0d25465303da0654a1fa2a69f6edb9
Mattias Ukusic <ukusic@gmail.com>
2016-11-20 07:46:45 +0000

    added vsan auto config

--- powershell/powercli/configure_deployed_vcsa_and_vesxi.ps1
+++ powershell/powercli/configure_deployed_vcsa_and_vesxi.ps1
@@ -9,6 +9,7 @@ $esxuser="root"
 $esxpass="X"
 $cluster="X"
 $datacenter="X"
+$VMKNetforVSAN = "Management Network"
 
 #### DO NOT EDIT BELOW ####
 
@@ -28,8 +29,8 @@ Catch
 
 # create datacenter and cluster
 $folder = Get-Folder -NoRecursion | Select -First 1
-New-DataCenter -Name $datacenter -Location $folder
-New-Cluster -Name $cluster -Location $datacenter
+$NewDatacenter =  New-DataCenter -Name $datacenter -Location $folder
+$NewCluster = New-Cluster -Name $cluster -Location $datacenter
 
 Write-Host "Created Datacenter: $datacenter ..."
 Write-Host "Created Cluster: $cluster ..."
@@ -37,27 +38,37 @@ Write-Host "Created Cluster: $cluster ..."
 # add hosts
 $cluster_ref = Get-Cluster $cluster
 
-$tasks = @()
 foreach($esxhost in $esxhosts) {
     Write-Host "Adding $esxhost to $cluster ..."
-    Add-VMHost -Name $esxhost -Location $cluster_ref -User $esxuser -Password $esxpass -Force | out-null
+    $AddedHost = Add-VMHost -Name $esxhost -Location $cluster_ref -User $esxuser -Password $esxpass -Force | out-null
+
+    # Check to see if they have a VSAN enabled VMKernel
+    $VMKernel = $AddedHost | Get-VMHostNetworkAdapter -VMKernel | Where {$_.PortGroupName -eq $VMKNetforVSAN }
+    $IsVSANEnabled = $VMKernel | Where { $_.VsanTrafficEnabled}
+    # If it isnt Enabled then Enable it
+    If (-not $IsVSANEnabled) {
+       Write-Host "Enabling VSAN Kernel on $VMKernel"
+       $VMKernel | Set-VMHostNetworkAdapter -VsanTrafficEnabled $true -Confirm:$false | Out-Null
+    } Else {
+       Write-Host "VSAN Kernel already enabled on $VmKernel"
+       $IsVSANEnabled | Select VMhost, DeviceName, IP, PortGroupName, VSANTrafficEnabled
+    }
+}
+
+# Enable VSAN on the cluster and set to Automatic Disk Claim Mode
+Write-Host "Enabling VSAN on $NewCluster"
+$VSANCluster = $NewCluster | Set-Cluster -VsanEnabled:$true -VsanDiskClaimMode Automatic -Confirm:$false -ErrorAction SilentlyContinue
+    If ($VSANCluster.VSANEnabled){
+       Write-Host "VSAN cluster $($VSANCLuster.Name) created in $($VSANCluster.VSANDiskClaimMode) configuration"
+       Write-Host "The following Hosts and Disk Groups now exist:"
+       Get-VsanDiskGroup | Select VMHost, Name | FT -AutoSize
+       Write-Host "The following VSAN Datastore now exists:"
+       Get-Datastore | Where {$_.Type -eq "vsan"} | Select Name, Type, FreeSpaceGB, CapacityGB
+} Else {
+    Write-Host "Something went wrong, VSAN not enabled"
 }
 
 Write-Host "VSAN Ready! But does not get enabled automagiskt ..."
-
-#$spec = New-Object VMware.Vim.ClusterConfigSpecEx
-#$vsanconfig = New-Object VMware.Vim.VsanClusterConfigInfo
-#$defaultconfig = New-Object VMware.Vim.VsanClusterConfigInfoHostDefaultInfo
-#$defaultconfig.AutoClaimStorage = $false
-#$vsanconfig.DefaultConfig = $defaultconfig
-#$vsanconfig.enabled = $true
-#$spec.VsanConfig = $vsanconfig
-
-#Write-Host "Enabling VSAN Cluster on $cluster ..."
-#$task = $cluster_ref.ExtensionData.ReconfigureComputeResource_Task($spec,$true)
-#$task1 = Get-Task -Id ("Task-$($task.value)")
-#$task1 | Wait-Task | out-null
-#Write-Host "OBS: You need to claim disks, autoclaim dont work without License key!"
 
 Try
 {
