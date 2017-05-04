ff9f36fdca551947cba0360fe814f72d245b8456
OKO <ukusic@gmail.com>
2017-05-04 20:26:15 +0200

    Create configure_deployed_vcsa_and_vesxi_no_vsan.ps1

---
+++ powershell/powercli/configure_deployed_vcsa_and_vesxi_no_vsan.ps1
@@ -0,0 +1,66 @@
+# Configure already deployed vcsa + x3 vesxi
+# Mattias Ukusic, 2016
+
+$vc="192.168.1.200"
+$vc_user="x"
+$vc_user_pass="x"
+$esxhosts=@("192.168.1.201", "192.168.1.202", "192.168.1.203")
+$esxuser="root"
+$esxpass="x"
+$cluster="LAB"
+$datacenter="Datacenter01"
+
+#### DO NOT EDIT BELOW ####
+
+# connect to vc
+Try
+{
+    Connect-VIServer $vc -User $vc_user -Password $vc_user_pass -ea Stop | Out-Null
+}
+Catch
+{
+    Write-Host "[Error] Can not connect to vCenter Server: $vc, exit script"
+    Exit
+}
+
+# create datacenter and cluster
+$folder = Get-Folder -NoRecursion | Select -First 1
+New-DataCenter -Name $datacenter -Location $folder
+New-Cluster -Name $cluster -Location $datacenter
+
+Write-Host "Created Datacenter: $datacenter ..."
+Write-Host "Created Cluster: $cluster ..."
+
+# add hosts
+$cluster_ref = Get-Cluster $cluster
+
+$tasks = @()
+foreach($esxhost in $esxhosts) {
+    Write-Host "Adding $esxhost to $cluster ..."
+    Add-VMHost -Name $esxhost -Location $cluster_ref -User $esxuser -Password $esxpass -Force | out-null
+}
+
+Write-Host "VSAN Ready! But does not get enabled automagiskt ..."
+
+#$spec = New-Object VMware.Vim.ClusterConfigSpecEx
+#$vsanconfig = New-Object VMware.Vim.VsanClusterConfigInfo
+#$defaultconfig = New-Object VMware.Vim.VsanClusterConfigInfoHostDefaultInfo
+#$defaultconfig.AutoClaimStorage = $false
+#$vsanconfig.DefaultConfig = $defaultconfig
+#$vsanconfig.enabled = $true
+#$spec.VsanConfig = $vsanconfig
+
+#Write-Host "Enabling VSAN Cluster on $cluster ..."
+#$task = $cluster_ref.ExtensionData.ReconfigureComputeResource_Task($spec,$true)
+#$task1 = Get-Task -Id ("Task-$($task.value)")
+#$task1 | Wait-Task | out-null
+#Write-Host "OBS: You need to claim disks, autoclaim dont work without License key!"
+
+Try
+{
+    Disconnect-VIServer * -Force -Confirm:$False
+}
+Catch
+{
+    # Nothing to do - no open connections!
+}
